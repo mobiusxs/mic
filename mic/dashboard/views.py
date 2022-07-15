b84c50d31b59b34e86dd3d37319e7023a1471e86
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-07-15 18:57:03 -0500

    add current user context

--- mic/dashboard/views.py
+++ mic/dashboard/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 
 from mic.auth.decorators import authentication_required
+from mic.auth.user import get_user
 
 dashboard = Blueprint('dashboard', __name__, template_folder='../templates')
 
@@ -8,4 +9,4 @@ dashboard = Blueprint('dashboard', __name__, template_folder='../templates')
 @dashboard.route('/')
 @authentication_required
 def index():
-    return render_template('dashboard/index.html')
+    return render_template('dashboard/index.html', user=get_user())
