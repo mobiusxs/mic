68a13521530961379cb5b4541c18dedf5ac66d9b
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-07-15 18:55:27 -0500

    register auth blueprint

--- mic/app.py
+++ mic/app.py
@@ -9,5 +9,8 @@ def create_app():
 
 
 def register_blueprints(app):
+    from mic.auth.views import auth
     from mic.dashboard.views import dashboard
+
+    app.register_blueprint(auth)
     app.register_blueprint(dashboard)
