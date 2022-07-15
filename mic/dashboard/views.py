c387f2d0f45e79d14f50eaba5a1312c091196850
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-07-15 18:56:51 -0500

    require authentication

--- mic/dashboard/views.py
+++ mic/dashboard/views.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
 
+from mic.auth.decorators import authentication_required
+
 dashboard = Blueprint('dashboard', __name__, template_folder='../templates')
 
 
 @dashboard.route('/')
+@authentication_required
 def index():
     return render_template('dashboard/index.html')
