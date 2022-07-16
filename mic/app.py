9709e6cf3620f44cf56608f8b9ee9bbb1aade7c9
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-07-16 04:23:11 -0500

    register extensions

--- mic/app.py
+++ mic/app.py
@@ -7,8 +7,16 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
 
+    register_extensions(app)
     register_blueprints(app)
     return app
+
+
+def register_extensions(app):
+    from mic.extensions import db, migrate
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
 
 def register_blueprints(app):
